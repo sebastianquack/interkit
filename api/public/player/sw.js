427263c04ffd3a5fcb56061797ffc83073795b7f
Sebastian Quack <Sebastian Quack>
2020-01-21 15:57:15 +0100

    player production build

--- api/public/player/sw.js
+++ api/public/player/sw.js
@@ -3,11 +3,5 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-  /*if (event.request.mode === 'navigate') {
-    event.respondWith(
-      new Response("<html><body>sorry, you need to be online</body></html>", {
-          headers: {'Content-Type': 'text/html; charset=UTF-8'}
-      })
-    );
-  }*/ 
+  console.log("fetch", event.request.url);
 });
