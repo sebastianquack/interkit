873a73a31dbd5ab203cac64be96d213d74780762
Sebastian Quack <Sebastian Quack>
2020-07-08 09:28:44 +0200

    production build

---
+++ api/public/admin/precache-manifest.d1cf80589db948267f73fd00117f255a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf67538eb8f386280d589464898443e",
+    "url": "/admin/index.html"
+  },
+  {
+    "revision": "63a4258f65832d3ea1a7",
+    "url": "/admin/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "179f74c66b34fd43efa5",
+    "url": "/admin/static/js/2.6d3c23b4.chunk.js"
+  },
+  {
+    "revision": "63a4258f65832d3ea1a7",
+    "url": "/admin/static/js/main.1982fa1d.chunk.js"
+  },
+  {
+    "revision": "7c4f45329e47798217a3",
+    "url": "/admin/static/js/runtime~main.b3695f6e.js"
+  }
+]);
