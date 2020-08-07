96cb8d43a5af4016777f791300195482a0594842
Holger H <holgheiss@gmail.com>
2020-08-07 17:52:31 +0200

    player: start botboot styles

--- player/src/App.svelte
+++ player/src/App.svelte
@@ -7,6 +7,7 @@
   import { onMount } from 'svelte';
   import { getConfig } from '../../shared/util.js';
   import PlayerContainer from './PlayerContainer.svelte';
+  import "./base.css";
 
   let projectId;
 
@@ -48,7 +49,6 @@
 
 
 <div class="app-container">
-<b>test</b>
 
 {#if projectId}   
 
@@ -67,25 +67,19 @@
 </div>
 
 
-<style type="text/scss">
+<style>
 
-$test: blue;
 
 .app-container {
-
     position: absolute;
     top: 0;
     width: 100%;
     height: 100%;
     max-width: 600px;
-    background-color: white;
+    background-color: var(--color-bright);
     left: 50%;
     transform: translate(-50%, 0);
     box-sizing: border-box;
-
-          b {
-      color: $test !important;
-      }
   }
 
 </style>
